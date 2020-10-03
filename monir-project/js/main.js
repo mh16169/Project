d4e25fc65bda57295419d7a09002b296f6a83a3f
Mahadi Hasan <42115266+mh16169@users.noreply.github.com>
2020-10-03 10:15:11 +0600

    Add files via upload

---
+++ monir-project/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+
+    // counterUp
+    $('.count').counterUp({
+        delay: 10,
+        time: 2000
+    });    
+
+    // mobile menu slick nav
+
+    $(function(){
+        $('.nav').slicknav();
+    });
+
+    // magnific pop up
+    
+
+
+
+
+
+});
